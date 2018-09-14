a2efba0f758a6071528fa5e2f83a755eec2b78d8
BuildTools <unconfigured@null.spigotmc.org>
2018-09-14 15:09:21 +0800

    Lab 2

--- Lab2/src/Bus.java
+++ Lab2/src/Bus.java
@@ -1,6 +1,11 @@
 public class Bus {
     private int busID;
+    private String model;
 
+    public Bus(int busID, String model){
+        this.busID=busID;
+        this.model=model;
+    }
     public int getID() {
         return busID;
     }
--- Lab2/src/BusCompany.java
+++ Lab2/src/BusCompany.java
@@ -1,12 +1,49 @@
 import java.util.Arrays;
 import java.util.Objects;
 
+
 public class BusCompany {
     private String name;
+    private int numBusses;
+    private static int numCompanies;
+    private int arraySize = 5;
+    private Bus[] busses;
 
     BusCompany(String name) {
         this.name = name;
         numCompanies++;
+        busses=new Bus[arraySize];
+    }
+
+    public String getName(){
+        String outname=new String(this.name);
+        return outname;
+    }
+
+    public boolean createAndAddBus(int id, String model){
+        Bus bus=new Bus(id, model);
+        for(int i=0;i<arraySize;i++){
+            if(busses[i]!=null) {
+                if (busses[i].getID() == id) {
+                    return false;
+                }
+            }
+        }
+        for(int i= 0;i<arraySize;i++){
+            if(busses[i]==null){
+                busses[i]=bus;
+                return true;
+            }
+        }
+        return false;
+    }
+    public void removeAllBusses(){
+        for(int i=0;i<arraySize;i++){
+            busses[i]=null;
+        }
+    }
+    public static int getNumCompanies(){
+        return numCompanies;
     }
 
 }
